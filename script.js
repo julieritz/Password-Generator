a832960e7f23232cd03dc781ae8920c2419a7976
julieritz <julie.ritz@gmail.com>
2020-03-20 10:09:44 -0700

    Added console.log

--- script.js
+++ script.js
@@ -8,88 +8,113 @@ function generatePassword() {
     // var passwordNumbers = confirm("Would you like to include numbers?");
     // var passwordSymbols = confirm("Would you like to include special characters?");
     
+    var password = ""
+
     var lowercase = "abcdefghijklmnopqrstuvwxyz";
     var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     var numbers = "0123456789";
     var symbols = "!#$%&\'()*+,-./:;<=>?@^[\\]^_`{|}~";
-    
-    var passwordLength;
-    var passwordUppercase;
-    var passwordLowercase;
-    var passwordNumbers;
-    var passwordSymbols;
-    
+
     var passwordLength = prompt("Please choose the desired length of your password, between 8-128 characters.");
-  
-    if (passwordLength<8){
-      alert("Password length must be between 8-128 characters");
-      determineLength();
-    }else if (passwordLength>128){
-      alert("Password length must be between 8-128 characters");
-      determineLength();
-    }else if (isNaN(passwordLength)){
-      alert("Password length must be a number between 8-128 characters");
-      determineLength();
+
+    if (passwordLength < 8) {
+        alert("Password length must be between 8-128 characters");
+        determineLength();
+    } else if (passwordLength > 128) {
+        alert("Password length must be between 8-128 characters");
+        determineLength();
+    } else if (isNaN(passwordLength)) {
+        alert("Password length must be a number between 8-128 characters");
+        determineLength();
     }
-    return passwordLength;
-  
-    passwordUppercase = confirm("Would you like to include uppercase letters?");
-  
-    if (passwordUppercase === "confirm"){
-      passwordUppercase = true;
-      return passwordUppercase;
-  
-    }else (passwordUppercase === "cancel"){
-      passwordUppercase = false;
-      return passwordUppercase;
+    console.log(passwordLength)
+
+    var passwordUppercase = confirm("Would you like to include uppercase letters?");
+    console.log(passwordUppercase)
+    // if (passwordUppercase === "confirm") {
+    //     passwordUppercase = true;
+    //     return passwordUppercase;
+
+    // } else if (passwordUppercase === "cancel") {
+    //     passwordUppercase = false;
+    //     return passwordUppercase;
+    // }
+
+    var passwordLowercase = confirm("Would you like to include lowercase letters?");
+    console.log(passwordLowercase)
+    // if (passwordLowercase === "confirm") {
+    //     passwordLowercase = true;
+    //     return passwordLowercase;
+
+    // } else if (passwordLowercase === "cancel") {
+    //     passwordLowercase = false;
+    //     return passwordLowercase;
+    // }
+
+    var passwordNumbers = confirm("Would you like to include numbers?");
+    console.log(passwordNumbers)
+    // if (passwordNumbers === "confirm") {
+    //     passwordNumbers = true;
+    //     return passwordNumbers;
+
+    // } else if (passwordNumbers === "cancel") {
+    //     passwordNumbers = false;
+    //     return passwordNumbers;
+    // }
+
+    var passwordSymbols = confirm("Would you like to include special characters?");
+    console.log(passwordSymbols)
+    // if (passwordSymbols === "confirm") {
+    //     passwordSymbols = true;
+    //     return passwordSymbols;
+
+    // } else if (passwordSymbols === "cancel") {
+    //     passwordSymbols = false;
+    //     return passwordSymbols;
+    // }
+
+    var passwordList = ""
+    if (passwordUppercase) {
+        passwordList += uppercase
+        console.log(passwordList)
     }
-      
-    passwordLowercase = confirm("Would you like to include lowercase letters?");
-  
-    if (passwordLowercase === "confirm"){
-      passwordLowercase = true;
-      return passwordLowercase;
-  
-    }else (passwordLowercase === "cancel"){
-      passwordLowercase = false;
-      return passwordLowercase;
+    if (passwordLowercase) {
+        passwordList += lowercase
+        console.log(passwordList)
     }
-  
-    passwordNumbers = prompt("Would you like to include numbers?");
-  
-    if (passwordNumbers === "confirm"){
-      passwordNumbers = true;
-      return passwordNumbers;
-  
-    }else if (passwordNumbers === "cancel"){
-      passwordNumbers = false;
-      return passwordNumbers;
+    if (passwordNumbers) {
+        passwordList += numbers
+        console.log(passwordList)
     }
-  
-    passwordSymbols = prompt("Would you like to include special characters?");
-  
-    if (passwordSymbols === "confirm"){
-      passwordSymbols = true;
-      return passwordSymbols;
-  
-    }else if (passwordSymbols === "cancel"){
-      passwordSymbols = false;
-      return passwordSymbols;
-      }
-  
-  //////////////////////////////////////////////////////////////
-  // DO NOT TOUCH THE CODE BELOW
-  //////////////////////////////////////////////////////////////
-  // Assignment Code
-  var generateBtn = document.querySelector("#generate");
-  
-  // Write password to the #password input
-  function writePassword() {
+    if (passwordSymbols) {
+        passwordList += symbols
+        console.log(passwordList)
+    }
+    console.log("passwordList ",passwordList)
+    if (passwordList.length<1){
+        alert ("You did not select any characters. Try again!")
+        return ""
+    }
+    for (var i = 0; i < passwordLength; i++) {
+        password += passwordList.charAt(Math.floor(Math.random() * passwordList.length));
+    }
+    console.log(password)
+      return password;
+}
+
+//////////////////////////////////////////////////////////////
+// DO NOT TOUCH THE CODE BELOW
+//////////////////////////////////////////////////////////////
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
     var password = generatePassword();
     var passwordText = document.querySelector("#password");
-  
+
     passwordText.value = password;
-  }
-  
-  // Add event listener to generate button
-  generateBtn.addEventListener("click", writePassword);
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
